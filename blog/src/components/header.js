2480c4e27bb0ad55f32b5243354d1c0795ea295c
Nicholas Claudio <nclaudio449@gmail.com>
2020-02-20 14:34:31 -0600

    Completed Activity 4

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -4,7 +4,7 @@ import React from "react"
 import styled from 'styled-components'
 
 const Outer = styled.header`
-  background: rebeccapurple;
+  background: ${props => props.theme.header.backgroundColor};
   margin-bottom: 1.45rem
 `
 
@@ -21,7 +21,7 @@ const StyledLink = styled(Link)`
   color: white;
   text-decoration: none;
   &:hover {
-    color: yellow;
+    color: purple;
   } 
 `
 
